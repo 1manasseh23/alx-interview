1061b4d3c4a72e62e3ec762c99e8a40976ce695f
1manasseh23 <cletavivan@gmail.com>
2024-07-04 08:22:41 +0000

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
+
+
 def canUnlockAll(boxes):
     """
     Determines if all the boxes can be opened.
-
     Args:
-        boxes (list): A list of lists, where each inner list represents the keys
-        in a particular box.
-
+        boxes
+    (list):
+        A list of lists, where each inner list represents
+        the keys in a particular box.
     Returns:
         bool: True if all boxes can be opened, False otherwise.
     """
+
     n = len(boxes)
     unlocked = [False] * n
-    unlocked[0] = True  # The first box is already unlocked
+    unlocked[0] = True
+    stack = [0]
 
-    stack = [0]  # Start with the first box
     while stack:
         current_box = stack.pop()
+
         for key in boxes[current_box]:
             if 0 <= key < n and not unlocked[key]:
                 unlocked[key] = True
