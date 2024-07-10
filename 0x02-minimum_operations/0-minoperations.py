e5575832fd235258193b0832496cad898db8c415
1manasseh23 <cletavivan@gmail.com>
2024-07-10 05:46:02 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,10 +9,12 @@ def minOperations(n):
         return 0
     # Find the largest power of 2 that is less
     # than or equal to n
+
     power_of_2 = 1
     while power_of_2 * 2 <= n:
         power_of_2 *= 2
     # Calculate the remaining operations needed
+
     remaining = n - power_of_2
     if remaining == 0:
         return power_of_2 // 1
