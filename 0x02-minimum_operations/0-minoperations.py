b858e3285f972f0661f4fbc94db5dc23d2d4e212
1manasseh23 <cletavivan@gmail.com>
2024-07-10 05:44:25 +0000

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Prototype: def minOperations(n)"""
+
+
+def minOperations(n):
+    """If n is impossible to achieve, return 0"""
+
+    if n <= 1:
+        return 0
+    # Find the largest power of 2 that is less
+    # than or equal to n
+    power_of_2 = 1
+    while power_of_2 * 2 <= n:
+        power_of_2 *= 2
+    # Calculate the remaining operations needed
+    remaining = n - power_of_2
+    if remaining == 0:
+        return power_of_2 // 1
+    else:
+        return power_of_2 // 1 + minOperations(remaining)
