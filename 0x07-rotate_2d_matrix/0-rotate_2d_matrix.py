db20af4a29f5a18103d61708fe2caeb6c57bae12
1manasseh23 <136644538+1manasseh23@users.noreply.github.com>
2024-08-14 08:13:57 -0700

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,8 +5,16 @@
 def rotate_2d_matrix(matrix):
     """Transpose the matrix"""
 
+    # This loop iterates over each row of the matrix
+    # The variable i represents the current row index 
     for i in range(len(matrix)):
+        # This loop starts from the next column of the current row and goes up to the last column
         for j in range(i, len(matrix)):
+            """
+            This line performs the actual swap between the elements at
+            positions (i, j) and (j, i). It uses tuple unpacking to simultaneously
+            assign new values to both variables
+            """
             matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
 
     # Reverse each row
